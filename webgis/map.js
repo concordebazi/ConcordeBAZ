5cb6a4303a19fc27dd410944f3e583cd3eea3edf
concordebazi <59648271+concordebazi@users.noreply.github.com>
2026-07-26 23:17:50 +0200

    Update map.js

--- webgis/map.js
+++ webgis/map.js
@@ -1,1 +1,74 @@
+// ==========================================
+// CONCORDE BAZIMAZIKI WEB GIS PROJECT
+// Leaflet + GeoJSON
+// ==========================================
 
+
+const map = L.map('map')
+.setView([55,15],5);
+
+
+
+
+// Base map
+
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+
+attribution:
+'© OpenStreetMap contributors'
+
+}
+
+).addTo(map);
+
+
+
+
+
+// Load GeoJSON layer
+
+
+fetch('data/locations.geojson')
+
+.then(response=>response.json())
+
+.then(data=>{
+
+
+L.geoJSON(data,{
+
+onEachFeature:function(
+feature,
+layer
+){
+
+
+layer.bindPopup(
+
+`
+
+<h3>
+${feature.properties.name}
+</h3>
+
+
+<p>
+
+${feature.properties.description}
+
+</p>
+
+`
+
+);
+
+
+}
+
+
+}).addTo(map);
+
+
+});
