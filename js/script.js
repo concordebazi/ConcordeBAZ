8ba6a76d0b2f823494d23738522b44e95a032222
concorde bazimaziki <59648271+concordebazi@users.noreply.github.com>
2026-07-29 13:23:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
+// ========================
+// IMAGE LIGHTBOX
+// ========================
 
+
+const images = document.querySelectorAll(".lightbox");
+
+const modal = document.getElementById("imageModal");
+
+const expandedImage =
+document.getElementById("expandedImage");
+
+const closeButton =
+document.querySelector(".close-modal");
+
+
+images.forEach(image => {
+
+
+image.addEventListener("click", function(e){
+
+e.preventDefault();
+
+
+modal.style.display="flex";
+
+
+expandedImage.src=this.href;
+
+
+});
+
+
+});
+
+
+closeButton.onclick=function(){
+
+modal.style.display="none";
+
+};
+
+
+modal.onclick=function(e){
+
+if(e.target === modal){
+
+modal.style.display="none";
+
+}
+
+};
