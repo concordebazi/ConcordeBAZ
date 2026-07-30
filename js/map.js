19fc7ecc7817c1cf78e8cc052daa341e3ede6c93
concorde bazimaziki <59648271+concordebazi@users.noreply.github.com>
2026-07-30 13:27:04 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -21,6 +21,62 @@ L.tileLayer(
         '&copy; OpenStreetMap contributors'
     }
 ).addTo(map);
+// ==========================================
+// PROFESSIONAL MAP ICONS
+// ==========================================
+
+
+var mapIcon = L.icon({
+
+iconUrl:
+'images/icons/map-icon.png',
+
+iconSize:
+[45,45],
+
+iconAnchor:
+[22,45],
+
+popupAnchor:
+[0,-40]
+
+});
+
+
+
+var geologyIcon = L.icon({
+
+iconUrl:
+'images/icons/geology-icon.png',
+
+iconSize:
+[45,45],
+
+iconAnchor:
+[22,45],
+
+popupAnchor:
+[0,-40]
+
+});
+
+
+
+var shipIcon = L.icon({
+
+iconUrl:
+'images/icons/ship-icon.png',
+
+iconSize:
+[45,45],
+
+iconAnchor:
+[22,45],
+
+popupAnchor:
+[0,-40]
+
+});
 
 // ==========================================
 // PROFESSIONAL MAP ICONS
